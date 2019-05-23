ba90d02acda968853128a5ef2abaff931f871da0
Mitch Tabian <mitch@tabian.ca>
2019-05-23 13:38:09 -0700

    test writing process

---
+++ app/src/test/java/com/codingwithmitch/unittesting2/models/NoteTest.java
@@ -0,0 +1,59 @@
+package com.codingwithmitch.unittesting2.models;
+
+import org.junit.jupiter.api.Test;
+
+public class NoteTest {
+
+    /*
+        Compare two equal Notes
+     */
+    @Test
+    void isNotesEqual_identicalProperties() throws Exception {
+
+        // Arrange
+        
+
+        // Act
+
+
+        // Assert
+
+    }
+
+
+    /*
+        Compare notes with 2 different ids
+     */
+
+    /*
+        Compare two notes with different timestamps
+     */
+
+
+    /*
+        Compare two notes with different titles
+     */
+
+
+    /*
+        Compare two notes with different content
+     */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
