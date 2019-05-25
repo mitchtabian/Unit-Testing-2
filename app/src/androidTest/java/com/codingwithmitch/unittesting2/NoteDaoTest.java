3a724509ba8df1efdad8deaef5455ba5c61bd314
Mitch Tabian <mitch@tabian.ca>
2019-05-25 15:17:42 -0700

    Room DAO Testing (part 1) (Instrumentation Test)

---
+++ app/src/androidTest/java/com/codingwithmitch/unittesting2/NoteDaoTest.java
@@ -0,0 +1,41 @@
+package com.codingwithmitch.unittesting2;
+
+public class NoteDaoTest extends NoteDatabaseTest {
+
+
+    /*
+        Insert, Read, Delete
+     */
+
+
+    /*
+        Insert, Read, Update, Read, Delete,
+     */
+
+
+    /*
+        Insert note with null title, throw exception
+     */
+
+
+    /*
+        Insert, Update with null title, throw exception
+     */
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
