b336a9bfdef15601bc6830e4c6dcacf36aaf3af6
Mitch Tabian <mitch@tabian.ca>
2019-05-26 17:01:44 -0700

    Unit Testing Data Insertion (Success, Fail, Exception)

--- app/src/test/java/com/codingwithmitch/unittesting2/repository/NoteRepositoryTest.java
+++ app/src/test/java/com/codingwithmitch/unittesting2/repository/NoteRepositoryTest.java
@@ -1,16 +1,26 @@
 package com.codingwithmitch.unittesting2.repository;
 
+import com.codingwithmitch.unittesting2.models.Note;
 import com.codingwithmitch.unittesting2.persistence.NoteDao;
+import com.codingwithmitch.unittesting2.ui.Resource;
+import com.codingwithmitch.unittesting2.util.TestUtil;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mockito;
 
+import io.reactivex.Single;
+
+import static com.codingwithmitch.unittesting2.repository.NoteRepository.INSERT_FAILURE;
+import static com.codingwithmitch.unittesting2.repository.NoteRepository.INSERT_SUCCESS;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class NoteRepositoryTest {
+
+    private static final Note NOTE1 = new Note(TestUtil.TEST_NOTE_1);
 
     // system under test
     private NoteRepository noteRepository;
@@ -29,15 +39,33 @@ public class NoteRepositoryTest {
         confirm observer is triggered
         confirm new rows inserted
      */
-
     @Test
     void insertNote_returnRow() throws Exception {
+
         // Arrange
+        final Long insertedRow = 1L;
+        final Single<Long> returnedData = Single.just(insertedRow);
+        when(noteDao.insertNote(any(Note.class))).thenReturn(returnedData);
 
         // Act
+        final Resource<Integer> returnedValue = noteRepository.insertNote(NOTE1).blockingSingle();
 
         // Assert
+        verify(noteDao).insertNote(any(Note.class));
+        verifyNoMoreInteractions(noteDao);
+
+        System.out.println("Returned value: " + returnedValue.data);
+        assertEquals(Resource.success(1, INSERT_SUCCESS), returnedValue);
+
+
+//        // Or test using RxJava
+//        noteRepository.insertNote(NOTE1)
+//                .test()
+//                .await()
+//                .assertValue(Resource.success(1, INSERT_SUCCESS));
     }
+
+
     /*
         Insert note
         Failure (return -1)
@@ -45,11 +73,20 @@ public class NoteRepositoryTest {
 
     @Test
     void insertNote_returnFailure() throws Exception {
+
         // Arrange
+        final Long failedInsert = -1L;
+        final Single<Long> returnedData = Single.just(failedInsert);
+        when(noteDao.insertNote(any(Note.class))).thenReturn(returnedData);
 
         // Act
+        final Resource<Integer> returnedValue = noteRepository.insertNote(NOTE1).blockingFirst();
 
         // Assert
+        verify(noteDao).insertNote(any(Note.class));
+        verifyNoMoreInteractions(noteDao);
+
+        assertEquals(Resource.error(null, INSERT_FAILURE), returnedValue);
     }
 
 
@@ -58,14 +95,17 @@ public class NoteRepositoryTest {
         null title
         confirm throw exception
      */
-
     @Test
     void insertNote_nullTitle_throwException() throws Exception {
-        // Arrange
 
-        // Act
-
-        // Assert
+        assertThrows(Exception.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                final Note note  = new Note(TestUtil.TEST_NOTE_1);
+                note.setTitle(null);
+                noteRepository.insertNote(note);
+            }
+        });
     }
 }
 
